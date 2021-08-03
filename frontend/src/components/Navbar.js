fe1608c979b37f30b892304937f3fd4669b77f16
SeungHoon Jang <jsh1400512@naver.com>
2021-08-04 00:21:05 +0900

    Add Screen

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import './Navbar.css';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar__logo">
+                <h2>Shopping Cart</h2>
+            </div>
+
+            <ul className="navbar__links">
+                <li>
+                    <Link to="/cart">
+                        
+                    </Link>
+                </li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import './HomeScreen.css';
+
+const HomeScreen = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default HomeScreen
