0e0ef324d09845156f634081ec012cbd4aa13ee0
SeungHoon Jang <jsh1400512@naver.com>
2021-08-04 13:24:10 +0900

    Add Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+// Components
+import Navbar from "./components/Navbar";
+import SideDrawer from "./components/SideDrawer";
+import Backdrop from "./components/Backdrop";
 
-import Navbar from './components/Navbar';
-
+// Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
-      <Navbar />
-      <main>
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <main className="app">
         <Switch>
-          <Route exact path="/" component={HomeScreen}/>
-          <Route exact path="/product/:id" component={ProductScreen}/>
-          <Route exact path="/cart" component={CartScreen}/>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
         </Switch>
       </main>
     </Router>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,35 +1,42 @@
-import './Navbar.css';
-import {Link} from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar__logo">
-                <h2>Shopping Cart</h2>
-            </div>
+const Navbar = ({ click }) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-            <ul className="navbar__links">
-                <li>
-                    <Link to="/cart" className="cart__link">
-                        <i className="fas fa-shopping-cart"></i>
-                        <span>
-                            Cart
-                            <span className="cartlogo__badge">0</span>
-                        </span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/">Shop</Link>
-                </li>
-            </ul>
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
 
-            <div className="hamburger__menu">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <h2>Shopping Cart</h2>
+      </div>
+
+      <ul className="navbar__links">
+        <li>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart <span className="cartlogo__badge">{getCartCount()}</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+
+      <div className="hamburger__menu" onClick={click}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
